2d3246fbf0a1d0d980338e91a2025bf7479209cf
rafael vincent <167981239+codenoob66@users.noreply.github.com>
2025-06-29 14:42:04 +0000

    hey

---
+++ stopwatch/script.js
@@ -0,0 +1,68 @@
+let minutesCount = 0;
+let secondsCount = 0;
+let hourCount = 0;
+let seconds = document.getElementById("seconds");
+let minutes = document.getElementById("minutes");
+let hour = document.getElementById("hour");
+let secondsIntervalId;
+const stopWatchBtn = document.getElementById("strtBtn");
+const endStopWatch = document.getElementById("endStopWatch");
+const docTitle = document.getElementById("docTitle");
+const pauseBtn = document.getElementById("pauseBtn");
+
+stopWatchBtn.addEventListener("click", function () {
+  console.log("started");
+  if (!secondsIntervalId) {
+    secondsIntervalId = setInterval(secondsFunction, 1000);
+  }
+});
+
+function secondsFunction() {
+  secondsCount++;
+  if (secondsCount === 60) {
+    secondsCount = 0;
+    minutesCount++;
+  }
+  if (minutesCount === 60) {
+    minutesCount = 0;
+    hourCount++;
+  }
+  seconds.textContent = padNum(secondsCount);
+  minutes.textContent = padNum(minutesCount);
+  hour.textContent = padNum(hourCount);
+  docTitle.textContent = `${hour.textContent}:${minutes.textContent}:${seconds.textContent}`;
+}
+endStopWatch.addEventListener("click", () => {
+  clearInterval(secondsIntervalId);
+  secondsIntervalId = null;
+  resetStopWatch();
+  docTitle.textContent = "Stop Watch";
+});
+
+function padNum(num, length = 2) {
+  return String(num).padStart(length, "0");
+}
+
+function resetStopWatch() {
+  minutesCount = 0;
+  minutes.textContent = padNum(minutesCount);
+  secondsCount = 0;
+  seconds.textContent = padNum(secondsCount);
+  hourCount = 0;
+  hour.textContent = padNum(hourCount);
+}
+
+function pauseStopWatch() {
+  if (pauseBtn.textContent == "pause") {
+    pauseBtn.textContent = "resume";
+    clearInterval(secondsIntervalId);
+    secondsIntervalId = null;
+  } else {
+    pauseBtn.textContent = "pause";
+    if (!secondsIntervalId) {
+      secondsIntervalId = setInterval(secondsFunction, 1000);
+    }
+  }
+}
+
+pauseBtn.addEventListener("click", pauseStopWatch);
